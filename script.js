565b7eab7283b102ff5bcfb13cf19f803926fbca
Yago Dini <yagoinha@gmail.com>
2026-06-02 15:25:44 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,8 @@
 (function () {
   var pdfBtn = document.getElementById('pdfBtn');
-  var sheet = document.getElementById('sheet');
-
-  if (pdfBtn && sheet && typeof html2pdf !== 'undefined') {
+  if (pdfBtn) {
     pdfBtn.addEventListener('click', function () {
-      pdfBtn.disabled = true;
-      document.body.classList.add('exporting');
-
-      var opt = {
-        margin: 8,
-        filename: 'yago-barbosa-dini.pdf',
-        image: { type: 'jpeg', quality: 0.98 },
-        html2canvas: { scale: 2, useCORS: true, backgroundColor: '#FBFAF6' },
-        jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' },
-        pagebreak: { mode: ['avoid-all', 'css', 'legacy'] }
-      };
-
-      html2pdf().set(opt).from(sheet).save().then(function () {
-        document.body.classList.remove('exporting');
-        pdfBtn.disabled = false;
-      }).catch(function () {
-        document.body.classList.remove('exporting');
-        pdfBtn.disabled = false;
-      });
+      window.print();
     });
   }
 
